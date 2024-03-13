a8f9adda8313ee62909d260ca1ee8dd28a313f15
Alexandru Buzescu <39965333+AlexGabi17@users.noreply.github.com>
2024-03-13 14:09:59 +0200

    Time: 0 ms (100.00%) | Memory: 8.3 MB (52.47%) - LeetSync

--- 1041-available-captures-for-rook/available-captures-for-rook.cpp
+++ 1041-available-captures-for-rook/available-captures-for-rook.cpp
@@ -1,7 +1,24 @@
 class Solution {
+    int countPawns(vector<vector<char>>& board, int temp_i, int temp_j, int step_i, int step_j){
+        temp_i = temp_i + step_i;
+        temp_j = temp_j + step_j;
+        int count = 0;
+        while(temp_i >= 0 && temp_i < board.size() && temp_j >=0 && temp_j < board.size()){
+            if(board[temp_i][temp_j] == 'p'){
+                count ++;
+                break;
+            }
+            else if(board[temp_i][temp_j] == 'B'){
+                break;
+            }
+            temp_i = temp_i + step_i;
+            temp_j = temp_j + step_j;
+        }
+        return count;
+    }
 public:
     int numRookCaptures(vector<vector<char>>& board) {
-        int rook_i, rook_j;
+        int rook_i, rook_j, count = 0;
         for(int i=0; i<board.size(); i++){
             for(int j=0; j<board.size(); j++){
                 if(board[i][j] == 'R'){
@@ -11,66 +28,19 @@ public:
                 }
             }
         }
-        cout<<rook_i<<" "<<rook_j;
-        int temp_i = rook_i, temp_j = rook_j, count = 0;
+        
         // atacking north
-        temp_i = temp_i - 1;
-        while(temp_i >= 0){
-            if(board[temp_i][temp_j] == 'p'){
-                count ++;
-                break;
-            }
-            else if(board[temp_i][temp_j] == 'B'){
-                break;
-            }
-            temp_i --;
-        }
+        count += countPawns(board, rook_i, rook_j, -1, 0);
 
         // attacking south
-
-        temp_i = rook_i, temp_j = rook_j;
-        temp_i = temp_i + 1;
-        while(temp_i < board.size()){
-            if(board[temp_i][temp_j] == 'p'){
-                count ++;
-                break;
-            }
-            else if(board[temp_i][temp_j] == 'B'){
-                break;
-            }
-            temp_i ++;
-        }
+        count += countPawns(board, rook_i, rook_j, 1, 0);
 
         // attacking west
-
-        temp_i = rook_i, temp_j = rook_j;
-        temp_j --;
-        while(temp_j >= 0){
-            if(board[temp_i][temp_j] == 'p'){
-                count ++;
-                break;
-            }
-            else if(board[temp_i][temp_j] == 'B'){
-                break;
-            }
-            temp_j --;
-        }
+        count += countPawns(board, rook_i, rook_j, 0, -1);
 
         // attacking east
+        count += countPawns(board, rook_i, rook_j, 0, 1);
 
-        temp_i = rook_i, temp_j = rook_j;
-        temp_j ++;
-        while(temp_j < board.size()){
-            if(board[temp_i][temp_j] == 'p'){
-                count ++;
-                break;
-            }
-            else if(board[temp_i][temp_j] == 'B'){
-                break;
-            }
-            temp_j ++;
-        }
-        
         return count;
 
     }
